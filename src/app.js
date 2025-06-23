03ada4ab1786dc5f0ee954b24165189b03c0f4b8
andr36s <andres40106@gmail.com>
2025-06-23 14:53:48 -0500

    12543 Agrego al app.js tasksRoutes y creo el modelo, la ruta y el controlador para las tareas

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import morgan from 'morgan';
 
+import tasksRoutes from './routes/tasks.routes.js'; 
+
 const app = express();
 
 app.use(morgan('dev')); // Logging middleware
+app.use(express.json()); // Middleware to parse JSON bodies
+
+app.use('/api', tasksRoutes); // Mounting tasks routes
 
 export default app;
 
---
+++ src/models/tasks.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const tasksSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
